5b3ddd95038f89119a3a5b7c7d7c63d6cc4c2b9e
mudassirsayed <sayedmudassir37@gmail.com>
2020-09-16 16:54:43 +0530

    Updates

---
+++ precache-manifest.14f137263d00b73885c0127c0eb37160.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1293065271faf3d48fe03eacc09f5bdb",
+    "url": "/react-api-project/index.html"
+  },
+  {
+    "revision": "2c52f5a568d8719d8526",
+    "url": "/react-api-project/static/css/main.3fe2f957.chunk.css"
+  },
+  {
+    "revision": "e83d7e1709342028231e",
+    "url": "/react-api-project/static/js/2.08afeb69.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-api-project/static/js/2.08afeb69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c52f5a568d8719d8526",
+    "url": "/react-api-project/static/js/main.b8375cf9.chunk.js"
+  },
+  {
+    "revision": "30a968625568950145d2",
+    "url": "/react-api-project/static/js/runtime-main.68f1307c.js"
+  }
+]);
